08c3165e2098bd975a5783f32aec171cc82908e6
SafeChild Development <admin@safechild.law>
2025-11-07 15:06:52 +0000

    auto-commit for 9549b15a-2974-4599-a4c4-71bf66dc51f3

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter, HTTPException, UploadFile, File, Form, Request
+from fastapi import FastAPI, APIRouter, HTTPException, UploadFile, File, Form, Request, Depends
 from fastapi.responses import FileResponse
 from dotenv import load_dotenv
 from starlette.middleware.cors import CORSMiddleware
